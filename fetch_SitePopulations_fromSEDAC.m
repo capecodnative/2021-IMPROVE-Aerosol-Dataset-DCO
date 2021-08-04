d6ff0563647d7f66378ff31037fc599759a31a7b
capecodnative <capecodnative@gmail.com>
2021-08-03 21:09:16 -0400

    Subdirectories for many figures
    Moved many figures into subdirectories

---
+++ fetch_SitePopulations_fromSEDAC.m
@@ -0,0 +1,15 @@
+SitePopulations100kmRad=nan(221,4);
+SiteLandArea100kmRad=nan(221,1);
+for i=1:221
+    tempCircle=fliplr(scircle1(SiteLat(i),SiteLon(i),50,[],earthRadius('km')));
+    tempRequest.polygon=tempCircle;
+    fprintf('Fetching #%d %s \n',i,SiteList(i));
+    tempResponse=webwrite('https://sedac.ciesin.columbia.edu/arcgis/rest/services/sedac/pesv3Broker/GPServer/pesv3Broker/execute',...
+        'Input_Data',jsonencode(tempRequest),'f','json',weboptions('Timeout',40));
+    tempResponseDecoded=jsondecode(tempResponse);
+    SitePopulations100kmRad(i,1)=str2double(tempResponseDecoded.results.value.estimates.gpw_v4_population_count_rev10_2000.SUM);
+    SitePopulations100kmRad(i,2)=str2double(tempResponseDecoded.results.value.estimates.gpw_v4_population_count_rev10_2005.SUM);
+    SitePopulations100kmRad(i,3)=str2double(tempResponseDecoded.results.value.estimates.gpw_v4_population_count_rev10_2015.SUM);
+    SitePopulations100kmRad(i,4)=str2double(tempResponseDecoded.results.value.estimates.gpw_v4_population_count_rev10_2020.SUM);
+    SiteLandArea100kmRad(i)=str2double(tempResponseDecoded.results.value.estimates.gpw_v4_land_water_area_rev10_landareakm.SUM);
+end
